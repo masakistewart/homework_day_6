3789438f7d0865c76c746fdaf62a08a78cdb82e2
Cairo Stewart <stewartcairo@gmail.com>
2015-10-26 16:23:45 -0700

    This is my Homework for the day

---
+++ nested.js
@@ -0,0 +1,11 @@
+var users = [user1, user2, user3, user4, user5]
+
+var listOfFirstnames = ["Kyle", "Kerrie", "Kruger", "KC", "Kromp"];
+var lastNames = ["Quincy", "Quaint", "Quale", "Quwibble", "Kwomp"];
+var parentsArr = ["Timmy Patty", ]
+
+for (var i = 0; i < users.length; i++) {
+	users[i].name = listOfFirstnames[i];
+	users[i].lastname = lastname[i];
+	users.parents[i] = parentsArr[i];
+};
