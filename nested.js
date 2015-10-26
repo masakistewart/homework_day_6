13f1f8aeac7ed82f5ef8b04ae11f32b324ecf1fd
Cairo Stewart <stewartcairo@gmail.com>
2015-10-26 16:25:43 -0700

    fixed the nested.js file

--- nested.js
+++ nested.js
@@ -1,11 +1,20 @@
+var user1 = {};
+var user2 = {};
+var user3 = {};
+var user4 = {};
+var user5 = {};
+
+
 var users = [user1, user2, user3, user4, user5]
 
 var listOfFirstnames = ["Kyle", "Kerrie", "Kruger", "KC", "Kromp"];
 var lastNames = ["Quincy", "Quaint", "Quale", "Quwibble", "Kwomp"];
-var parentsArr = ["Timmy Patty", ]
 
-for (var i = 0; i < users.length; i++) {
-	users[i].name = listOfFirstnames[i];
-	users[i].lastname = lastname[i];
-	users.parents[i] = parentsArr[i];
-};
+var parentsArr = ["Timmy Patty", "James Jessy", "Matt Alex", "Pats Jess", "Fumi Scott" ];
+
+
+ for (var i = 0; i < users.length; i++) {
+	users[i].firstName = listOfFirstnames[i];
+	users[i].lastname = lastNames[i];
+	users[i].parents = parentsArr[i];
+ }
